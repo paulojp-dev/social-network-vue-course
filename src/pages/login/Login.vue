01c1781e39355677de8f823625d6c8d30e0c201c
João Paulo Ferreira da Silva <joao.paulo@zage.com.br>
2019-06-01 15:32:25 -0300

    Define templates for site and login

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    <login-template>
+      <h2>Login</h2>
+
+      <label for="loginEmailID">Email</label>
+      <input id="loginEmailID" type="text" placeholder="E-mail" value="" />
+      <label for="loginPasswordID">Password</label>
+      <input id="loginPasswordID" type="password" placeholder="Password" value="" />
+
+      <button class="btn">Sing In</button>
+    </login-template>
+  </span>
+</template>
+
+<script>
+import LoginTemplate from '@/templates/LoginTemplate'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  },
+  components: {
+    LoginTemplate
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/templates/LoginTemplate.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header>
+      <nav-bar-vue logo="Social" url="#" color="blue"></nav-bar-vue>
+    </header>
+
+    <main>
+      <div class="container">
+        <div class="row">
+          <grid-vue size="6">
+            <card-menu-vue>
+            </card-menu-vue>
+          </grid-vue>
+
+          <grid-vue size="6">
+            <slot></slot>
+          </grid-vue>
+        </div>
+      </div>
+    </main>
+
+    <footer-vue color="blue" logo="Social" description="Rede social" ano="2019">
+      <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+    </footer-vue>
+  </div>
+</template>
+
+<script>
+import NavBarVue from '@/components/layouts/NavBarVue'
+import FooterVue from '@/components/layouts/FooterVue'
+import GridVue from '@/components/layouts/GridVue'
+import CardMenuVue from '@/components/layouts/CardMenuVue'
+
+export default {
+  name: 'LoginTemplate',
+  components: {
+    NavBarVue,
+    FooterVue,
+    GridVue,
+    CardMenuVue
+  }
+}
+</script>
+
+<style>
+</style>
