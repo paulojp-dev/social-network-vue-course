17ee0422f566639aa94000c3472ea2e90609295a
João Paulo Ferreira da Silva <joao.paulo@zage.com.br>
2019-06-02 01:55:28 -0300

    Integrate login with Api

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -12,11 +12,11 @@
          <h2>Login</h2>
 
           <label for="loginEmailID">Email</label>
-          <input id="loginEmailID" type="text" placeholder="E-mail" value="" />
+          <input id="loginEmailID" type="text" placeholder="E-mail" v-model="user.email" />
           <label for="loginPasswordID">Password</label>
-          <input id="loginPasswordID" type="password" placeholder="Password" value="" />
+          <input id="loginPasswordID" type="password" placeholder="Password" v-model="user.password" />
 
-          <button class="btn">Sing In</button>
+          <button class="btn" v-on:click="login">Sing In</button>
           <router-link class="btn orange" to="/register">Register</router-link>
       </span>
     </login-template>
@@ -25,15 +25,49 @@
 
 <script>
 import LoginTemplate from '@/templates/LoginTemplate'
+import axios from 'axios'
 
 export default {
   name: 'Login',
   data () {
     return {
+      user: {
+        'email': '',
+        'password': ''
+      }
     }
   },
   components: {
     LoginTemplate
+  },
+  methods: {
+    login () {
+      axios.post('http://127.0.0.1:8000/api/login', {
+        email: this.user.email,
+        password: this.user.password
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        if (response.status === 200) {
+          console.log('successfully logged in!')
+        } else {
+          console.log('StatusCode ' + response.status + ' not expected')
+        }
+      }).catch(e => {
+        switch (e.response.status) {
+          case 423:
+            console.log('Validation error')
+            break
+          case 500:
+            console.log('API internal error')
+            break
+          default:
+            console.log('StatusCode ' + e.response.status + ' not expected')
+        }
+      })
+    }
   }
 }
 </script>
