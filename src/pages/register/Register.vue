a7bb6fb696e63b4167574811d2ad79bec7d83327
João Paulo Ferreira da Silva <paulojp.dev@gmail.com>
2019-06-02 19:50:30 -0300

    register page

--- src/pages/register/Register.vue
+++ src/pages/register/Register.vue
@@ -12,15 +12,16 @@
         <h2>Register</h2>
 
           <label for="registerNameID">Name</label>
-          <input id="registerNameID" type="text" placeholder="Full name" value="" />
-          <label for="registerEmailID">Email</label>
-          <input id="registerEmailID" type="text" placeholder="E-mail" value="" />
+          <input id="registerNameID" placeholder="Full name" type="text" v-model="user.name" />
+           <label for="registerEmailID">Email</label>
+          <input id="registerEmailID" type="text" placeholder="E-mail" v-model="user.email" />
           <label for="registerPasswordID">Password</label>
-          <input id="registerPasswordID" type="password" placeholder="Password" value="" />
+          <input id="registerPasswordID" placeholder="Password" type="password" v-model="user.password" />
           <label for="registerConfirmPasswordID">Confirm Password</label>
-          <input id="registerConfirmPasswordID" type="password" placeholder="Password confirmation" value="" />
+          <input id="registerConfirmPasswordID" placeholder="Password confirmation" type="password"
+                 v-model="user.password_confirmation" />
 
-          <button class="btn">Register</button>
+          <button class="btn" v-on:click="register()">Register</button>
           <router-link class="btn blue" to="/login">I'm already registered</router-link>
       </span>
     </login-template>
@@ -29,15 +30,61 @@
 
 <script>
 import LoginTemplate from '@/templates/LoginTemplate'
+import axios from 'axios'
 
 export default {
   name: 'Register',
   data () {
     return {
+      'user': {
+        'name': '',
+        'email': '',
+        'password': '',
+        'password_confirmation': ''
+      }
     }
   },
   components: {
     LoginTemplate
+  },
+  methods: {
+    register () {
+      axios.post('http://127.0.0.1:8000/api/register', {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password,
+        password_confirmation: this.user.password_confirmation
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Cache-Control': 'no-cache'
+        }
+      }).then(response => {
+        if (response.status >= 200 && response.data.token) {
+          this.$router.push('/login')
+        } else {
+          console.log('statusCode ' + response.status + ' not expected')
+        }
+      }).catch(e => {
+        switch (e.response.status) {
+          case 423:
+            let errors = ''
+            for (let error of Object.values(e.response.data)) {
+              errors += error + ' '
+            }
+            alert(errors)
+            break
+          case 403:
+            console.log('invalid credentials!')
+            break
+          case 500:
+            console.log('API internal error')
+            break
+          default:
+            console.log('statusCode ' + e.response.status + ' not expected')
+        }
+      })
+    }
   }
 }
 </script>
